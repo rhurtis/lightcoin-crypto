aa659cd8f6d4aa136a9122aa0bf5c678da3c675a
Ryan H <rhurtis@hotmail.com>
2020-08-03 06:47:49 +0000

    Using object oriented programming to mimic transactions.

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-let balance = 500.00;
+//let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+  commit() {
+    this.account.balance += this.value;
   }
 
-  commit() {
-    balance -= this.amount;
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
   }
 
 }
 
 
-
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+
+let t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+let t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+let t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+
+
+
+console.log('Balance:', myAccount['balance']);
